f6ccd702b28c23d1893f105de664b36c97edd120
Ali Raheem <github@shoryuken.me>
2020-05-27 09:50:41 +0100

    Use openmp

--- src/xorioc.c
+++ src/xorioc.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 int main(int argc, char *argv[]) {
   if (3 != argc) {
@@ -10,11 +13,16 @@ int main(int argc, char *argv[]) {
 
   char *filename = argv[1];
   int maxlen = atoi(argv[2]);
-  
+
   FILE *fp = fopen(filename, "rb");
+  struct stat fpstat;
+  stat(filename, &fpstat);
+  size_t filesize = fpstat.st_size; 
+/*
   fseek(fp, 0L, SEEK_END);
   size_t filesize = ftell(fp);
   rewind(fp);
+*/
 
   char *buffer = (char *) malloc(filesize);
   fread(buffer, filesize, 1, fp);
@@ -23,18 +31,26 @@ int main(int argc, char *argv[]) {
     maxlen = filesize - 1;
   }
 
+
+  float *ioc = (float *)malloc(sizeof (float) * maxlen);
+  float ioc_total;
+  uint total, matchs;
   int i, j, k;
+# pragma omp parallel for private(total, matchs, j, k) reduction(+:ioc_total)
   for(i = maxlen; i > 0; i--) {
-    uint total = 0;
-    uint matchs = 0;
+    total = 0;
+    matchs = 0;
     for(j = 0; j < filesize; j++) {
       for(k = i + j; k < filesize; k += i) {
 	total++;
 	if(buffer[j] == buffer[k]) matchs++;
       }
     }
-    float ioc = 100.0 * (float) matchs / (float) total;
-    printf("%04d %0.3f\% \n", i, ioc);
+    ioc[i] = (float) matchs / (float) total;
+    ioc_total += ioc[i];
   }
-  
+
+  for(i = 1; i < maxlen + 1; i++) {
+    printf("%04d %0.3f \n", i, maxlen*ioc[i]/ioc_total);
+  }
 }
